54756d8cdba936a5f657ae5d696efd6584e41476
BjornFelix <bjorn11.felix@gmail.com>
2022-04-08 11:57:33 +0200

    Basic login logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 void main() {
+  Firebase.initializeApp(options:const FirebaseOptions(apiKey:'AIzaSyDmvRKhdCnPR1ZmUmJ6YoNzFZ9wx4998D0' , appId: 'project-425922472925', messagingSenderId: '425922472925', projectId: 'intromobile-740ff') );
   runApp(const MyApp());
 }
 
@@ -30,7 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,44 +40,31 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-
-
-          child: Row(
-            
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const AdminRoute()),
-                  );
-                },
-
-                child: const Text("Admin"),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red[800],
-                  padding: const EdgeInsets.all(100.0),
-              
-                ),
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AdminRoute()),
+                );
+              },
+              child: const Text("Admin"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                padding: const EdgeInsets.all(100.0),
               ),
-
-              ElevatedButton(
-          
-                onPressed: () {
-                },
-
-                child: const Text("Student"),
-                style: ElevatedButton.styleFrom(
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Student"),
+              style: ElevatedButton.styleFrom(
                   primary: Colors.red[800],
-                  padding: const EdgeInsets.all(100.0)
-                ),
-              ),
+                  padding: const EdgeInsets.all(100.0)),
+            ),
           ],
-          )
-           
-        ));
+        )));
   }
 }
 
@@ -86,50 +74,116 @@ class AdminRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[800],
-        title: const Text('Admin Route'),
-      ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-
-                child: const Text('Examenvragen aanmaken'),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.red[800],
-                    padding: const EdgeInsets.fromLTRB(300.0,75.0,300.0,75.0)
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-
-                child: const Text('Studentenlijst'),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.red[800],
-                  padding: const EdgeInsets.fromLTRB(338.0,75.0,338.0,75.0)
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-
-                child: const Text('Wachtwoord Wijzigen'),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.red[800],
-                  padding: const EdgeInsets.fromLTRB(315.0,75.0,315.0,75.0)
-                ),
-              ),
-            ],
-        )
-      )
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.red[800],
+          title: const Text('Admin Route'),
+        ),
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context,snapshot){
+            if (snapshot.hasData) {
+              return Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Examenvragen aanmaken'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.red[800],
+                  padding: const EdgeInsets.fromLTRB(300.0, 75.0, 300.0, 75.0)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Studentenlijst'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.red[800],
+                  padding: const EdgeInsets.fromLTRB(338.0, 75.0, 338.0, 75.0)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Wachtwoord Wijzigen'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.red[800],
+                  padding: const EdgeInsets.fromLTRB(315.0, 75.0, 315.0, 75.0)),
+            ),
+          ],
+        ));
+            }
+            else{
+              return const LoginWidget();
+            }
+          }
+        ) 
+        );
   }
+}
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40
+            ),
+            TextField(
+                controller: emailController,
+                cursorColor: Colors.white,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(labelText: "Email")),
+            const SizedBox(height: 4),
+            TextField(
+              controller: passwordController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+           const SizedBox(height: 20),
+            ElevatedButton.icon(
+                style:
+                    ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+                onPressed: signIn,
+                icon: const Icon(
+                  Icons.lock_open,
+                  size: 32,
+                ),
+                label: const Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 24),
+                ))
+          ],
+        ));
+  }
+  Future signIn()async{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim());
+  }
+
 }
