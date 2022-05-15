20c08583dab61103463d10a96940582675f8a38b
Daniel Awan Gonçalves <s121006@ap.be>
2022-05-16 00:03:57 +0200

    Attempt to fix correction of exams

--- lib/Admin/CorrectExam.dart
+++ lib/Admin/CorrectExam.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:csv/csv.dart' as csv;
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../Student/AnswerQuestion.dart';
@@ -18,8 +19,11 @@ class CorrectExam extends StatefulWidget {
   State<CorrectExam> createState() => _CorrectExamState();
 }
 
+
+
 class _CorrectExamState extends State<CorrectExam> {
-int score=0;
+  int score = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +34,15 @@ int score=0;
               Text('Selecteer student'),
             ],
           )),
-      body: StreamBuilder<List<Exam>>(
-          stream: getExam(widget.student),
+      body: FutureBuilder<List<Exam>>(
+          future: getExam(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
+              debugPrint(snapshot.error.toString());
               return const Text('Een fout heeft zich plaatsgevonden');
             } else if (snapshot.hasData) {
               final exam = snapshot.data!;
-final answers = exam[0].studentAnswers;
+              final answers = exam[0].studentAnswers;
               return ListView(
                   children: answers.map((e) => buildExam(e)).toList());
             } else {
@@ -50,49 +55,47 @@ final answers = exam[0].studentAnswers;
   }
 
   Widget buildExam(StudentAnswer answer) {
-    int points= answer.question.points;
+    int points = answer.question.points;
 
-if (answer.question.type=='CQ'||answer.question.type=='CC' ||answer.question.type=='MC') {
-    if (answer.answer.toUpperCase()==answer.question.answer) {
-      score+=points;
+    if (answer.question.type == 'CQ' || answer.question.type == 'CC' ||
+        answer.question.type == 'MC') {
+      if (answer.answer.toUpperCase() == answer.question.answer) {
+        score += points;
         return ListTile(
-      
-        title: Text(answer.question.question),
-        subtitle: Text(answer.answer+ points.toString()+"/"+points.toString())
-        );
-    }
-    else{
-    return ListTile(
-      
-        title: Text(answer.question.question),
-        subtitle: Text(answer.answer + "0/"+ answer.question.points.toString())
-        );
 
+            title: Text(answer.question.question),
+            subtitle: Text(
+                answer.answer + points.toString() + "/" + points.toString())
+        );
+      }
+      else {
+        return ListTile(
+
+            title: Text(answer.question.question),
+            subtitle: Text(
+                answer.answer + "0/" + answer.question.points.toString())
+        );
+      }
     }
-  }
- return ListTile(
-      
-      onTap: (){},
+    return ListTile(
+
+        onTap: () {},
         title: Text(answer.question.question),
         subtitle: Text(answer.answer)
-        );
+    );
   }
 
 
-
-
-  Stream<List<Exam>> getExam(Student student) {
-    print(student.id);
-    Stream<List<Exam>> exam= FirebaseFirestore.instance
-        .collection("exams")
-        .where("studentId", isEqualTo: student.id)
-        .snapshots()
-        .map((snapshot) => snapshot.docs
-            .map((doc) => Exam.fromJson(doc.data(), doc.id))
-            .toList());
-
-print(exam.toString());
-          return exam;
+  Future <List<Exam>> getExam() async {
+    var collection = FirebaseFirestore.instance.collection('exams');
+    var querySnapshot = await collection.get();
+    List<Exam> result = [];
+    for (var queryDocumentSnapshot in querySnapshot.docs) {
+      Map<String, dynamic> data = queryDocumentSnapshot.data();
+      data["studentId"] = queryDocumentSnapshot.id;
+      result.add(Exam.fromJson(data, queryDocumentSnapshot.id));
+      }
+    print(result.toString());
+    return result;
   }
-
 }
